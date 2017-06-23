a3a5dafbddcb73c7ea933789b3ec109016145329
mjholder <mjholder96@gmail.com>
2017-06-23 15:17:56 -0400

    Added config.json to modify url host

--- jobsearch/__init__.py
+++ jobsearch/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, redirect
+import json
 
 app = Flask(__name__)
 
+try:
+    conf = open('jobsearch/statics/config.json')
+    hostname = json.load(conf)['hostname']
+except:
+    hostname = 'pcp2.ccr.buffalo.edu:8080'
+
 @app.route('/')
 def main():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', host=hostname)
 
 @app.route('/search', methods=['GET'])
 def input():
     cluster = request.args['cluster']
     job = request.args['jobid']
-    url = "http://pcp2.ccr.buffalo.edu:8080/supremm_rest/loader.html?resource_id=" + cluster + "&jobid=" + job
+    url = "http://" + hostname + "/supremm_rest/loader.html?resource_id=" + cluster + "&jobid=" + job
     print url
     return redirect(302, url)
